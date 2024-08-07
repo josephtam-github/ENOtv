2c705db4c291f5d1700c905df094ee59b8af77f1
Joseph Tam <80483702+josephtam-github@users.noreply.github.com>
2024-08-07 06:09:25 +0100

    fix: specify correct file name to render template

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def login_page():
 
 @app.route('/signup')
 def signup_page():
-    return render_template('signup.html')
+    return render_template('register.html')
 
 @app.route('/browse')
 def browse_page():
